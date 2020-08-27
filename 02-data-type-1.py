6b8e85f0d9edb7543500479248ffcc3ecee9b39d
hyunyoungkim651 <k00810111@gmail.com>
2020-08-27 10:59:58 +0900

    Add python study

---
+++ 02-data-type-1.py
@@ -0,0 +1,20 @@
+# 정수 int
+print(1 + 1)
+# 소수 float
+print(1.5 + 2.1)
+# 문자 string
+print("hello")
+
+# print("hello" + "1")
+
+# print(4 // 2)
+# print(int(4 / 2))
+# print(float(2))
+
+# 문자
+print("1")
+
+# 숫자
+print(int("1"))
+
+print(int(1.5))
