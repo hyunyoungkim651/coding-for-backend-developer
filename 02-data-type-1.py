5e4c8c3239b60cbc8500f4fe9b1a6aab24bda349
hyunyoungkim651 <k00810111@gmail.com>
2020-08-27 14:54:12 +0900

    python study

--- 02-data-type-1.py
+++ 02-data-type-1.py
@@ -2,19 +2,20 @@
 print(1 + 1)
 # 소수 float
 print(1.5 + 2.1)
-# 문자 string
+# 문자 str - string
 print("hello")
 
-# print("hello" + "1")
+# 문자열 더하기
+print("hello" + "1")
 
-# print(4 // 2)
-# print(int(4 / 2))
-# print(float(2))
+# 정수 소수 간 변경
+print(4 // 2)
+print(int(4 / 2))
+print(float(2))
 
-# 문자
+# 문자열 숫자 간 변경
 print("1")
-
-# 숫자
 print(int("1"))
 
+# 내림해서 정수 1로 나온다.
 print(int(1.5))
---
+++ 04-if-statement.py
@@ -0,0 +1,28 @@
+# 참과 거짓 boolean
+# if
+# True, False
+# and, or, not
+
+# a = False
+# b = True
+# # A가 참이고 그리고 B가 참이라면 (A와 B 둘다 참이여야 한다.)
+# print(a and b)
+# # A가 참이거나 혹은 B가 참이라면 (A나 B 둘 중 하나라도 참이면 된다.)
+# print(a or b)
+
+
+# # = : 대입
+# c = True
+# # == : 같은가
+# a == True
+# print(a == True)
+# print(a is True)
+
+# if
+d = 7
+if d > 10:
+    print("숫자는 10보다 큽니다")
+elif d > 5 and d <= 10:
+    print("숫자는 10보다 작거나 같고, 5보다는 큽니다.")
+else:
+    print("숫자는 5보다 작거나 같습니다.")
